a89677f07da3baf2004dbf31394446d3b1f2d645
Mikołaj Makowski <308125@o365.stud.umk.pl>
2023-06-10 22:36:08 +0200

    Add tests for pull_request.opened and team.deleted

--- test/index.test.ts
+++ test/index.test.ts
@@ -4,9 +4,18 @@ import nock from "nock";
 // Requiring our app implementation
 import myProbotApp from "../src";
 import { Probot, ProbotOctokit } from "probot";
+
 // Requiring our fixtures
-import teamCreatedPayload from "./fixtures/team.created.json";
-import projectIdQueryResponse from "./fixtures/projectId.query.response.json"
+import payloadTeamCreated from "./fixtures/payloads/team.created.json";
+import payloadTeamDeleted from "./fixtures/payloads/team.deleted.json";
+import payloadPullRequestOpened from "./fixtures/payloads/pull_request.opened.json"
+
+import responseProjectsInOrg from "./fixtures/api_responses/graphql_queries/projectsInOrg.json"
+import responseProjectId from "./fixtures/api_responses/graphql_queries/projectId.json";
+import responseProjectDelete from "./fixtures/api_responses/graphql_mutations/closeProject.json"
+import responseUserTeamsInOrg from "./fixtures/api_responses/graphql_queries/userTeamsInOrg.json"
+import responseAddItemToProj from "./fixtures/api_responses/graphql_mutations/addItemToProj.json"
+import responseUpdateDateField from "./fixtures/api_responses/graphql_mutations/updateItemDateField.json"
 
 import fs from "fs";
 import path from "path";
@@ -50,60 +59,118 @@ describe("Czujnikownia app tests", () => {
 
     test("test creating projects with settings from .github-private", async () => {
       const mock = nock("https://api.github.com")
-        .get(`/repos/${teamCreatedPayload.organization.login}/.github-private/contents/${encodeURIComponent(".github/sensor-room.yml")}`)
+        .get(`/repos/${payloadTeamCreated.organization.login}/.github-private/contents/${encodeURIComponent(".github/sensor-room.yml")}`)
         .reply(200, fs.readFileSync(path.join(__dirname, "fixtures/sensor-room.yml"), "utf-8"))
         .post("/graphql", (body) => {
-          expect(body.variables.organizationLogin).toEqual(teamCreatedPayload.organization.login);
+          expect(body.variables.organizationLogin).toEqual(payloadTeamCreated.organization.login);
           expect(body.variables.projectNumber).toEqual(6);
           return true;
         })
-        .reply(200, projectIdQueryResponse)
+        .reply(200, responseProjectId)
         .post("/graphql", (body) => {
-          expect(body.variables.projectId).toEqual(projectIdQueryResponse.data.organization.projectV2.id);
-          expect(body.variables.ownerId).toEqual(teamCreatedPayload.organization.node_id);
+          expect(body.variables.projectId).toEqual(responseProjectId.data.organization.projectV2.id);
+          expect(body.variables.ownerId).toEqual(payloadTeamCreated.organization.node_id);
           expect(body.variables.title).toEqual("monitor-test-team-2023");
           return true;
         })
         .reply(200);
 
-      await probot.receive({ name: "team.created", payload: teamCreatedPayload });
+      await probot.receive({ name: "team.created", payload: payloadTeamCreated });
     
       expect(mock.pendingMocks()).toStrictEqual([]);
     });
 
     test("test creating projects with settings from .github-private, but template project doesn't exists", async () => {
       const mock = nock("https://api.github.com")
-        .get(`/repos/${teamCreatedPayload.organization.login}/.github-private/contents/${encodeURIComponent(".github/sensor-room.yml")}`)
+        .get(`/repos/${payloadTeamCreated.organization.login}/.github-private/contents/${encodeURIComponent(".github/sensor-room.yml")}`)
         .reply(200, fs.readFileSync(path.join(__dirname, "fixtures/sensor-room.yml"), "utf-8"))
         .post("/graphql", (body) => {
-          expect(body.variables.organizationLogin).toEqual(teamCreatedPayload.organization.login);
+          expect(body.variables.organizationLogin).toEqual(payloadTeamCreated.organization.login);
           expect(body.variables.projectNumber).toEqual(6);
           return true;
         })
         .reply(404);
         
-      await probot.receive({ name: "team.created", payload: teamCreatedPayload });
+      await probot.receive({ name: "team.created", payload: payloadTeamCreated });
     
       expect(mock.pendingMocks()).toStrictEqual([]);
     });
 
     test("test creating projects with default settings", async () => {
       const mock = nock("https://api.github.com")
-        .get(`/repos/${teamCreatedPayload.organization.login}/.github-private/contents/${encodeURIComponent(".github/sensor-room.yml")}`)
+        .get(`/repos/${payloadTeamCreated.organization.login}/.github-private/contents/${encodeURIComponent(".github/sensor-room.yml")}`)
         .reply(404)
-        .get(`/repos/${teamCreatedPayload.organization.login}/.github/contents/${encodeURIComponent(".github/sensor-room.yml")}`)
+        .get(`/repos/${payloadTeamCreated.organization.login}/.github/contents/${encodeURIComponent(".github/sensor-room.yml")}`)
         .reply(404)
         .post("/graphql", (body) => {
-          expect(body.variables.ownerId).toEqual(teamCreatedPayload.organization.node_id)
+          expect(body.variables.ownerId).toEqual(payloadTeamCreated.organization.node_id)
           expect(body.variables.title).toEqual("sensor-test-team-2023")
           return true;
         })
         .reply(200)
 
-      await probot.receive({ name: "team.created", payload: teamCreatedPayload });
+      await probot.receive({ name: "team.created", payload: payloadTeamCreated });
       
       expect(mock.pendingMocks()).toStrictEqual([]);
-    })
+    });
+
+    test("test closing project", async () => {
+      const mock = nock("https://api.github.com")
+        .get(`/repos/${payloadTeamDeleted.organization.login}/.github-private/contents/${encodeURIComponent(".github/sensor-room.yml")}`)
+        .reply(200, fs.readFileSync(path.join(__dirname, "fixtures/sensor-room.yml"), "utf-8"))
+        
+        .post("/graphql", (body) => {
+          expect(body.variables.organizationLogin).toEqual(payloadTeamDeleted.organization.login)
+          return true;
+        })
+        .reply(200, responseProjectsInOrg)
+        .post("/graphql", (body) => {
+          expect(body.variables.projectId).toEqual("PVT_kwDOB8o1Ys4APTCq")
+          return true;
+        })
+        .reply(200, responseProjectDelete)
+
+        await probot.receive({ name: "team.deleted", payload: payloadTeamDeleted });
+        expect(mock.pendingMocks()).toStrictEqual([]);
+    });
+
+    test("saving pull request date in project", async () => {
+      const mock = nock("https://api.github.com")
+        .post("/graphql", (body) => {
+          expect(body.variables.userLogin).toEqual(payloadPullRequestOpened.sender.login)
+          expect(body.variables.organizationLogin).toEqual(payloadPullRequestOpened.organization.login)
+          return true;
+        })
+        .reply(200, responseUserTeamsInOrg)
+
+        .post("/graphql", (body) => {
+          expect(body.variables.organizationLogin).toEqual(payloadPullRequestOpened.organization.login)
+          return true;
+        })
+        .reply(200, responseProjectsInOrg)
+
+        .get(`/repos/${payloadTeamDeleted.organization.login}/.github-private/contents/${encodeURIComponent(".github/sensor-room.yml")}`)
+        .reply(200, fs.readFileSync(path.join(__dirname, "fixtures/sensor-room.yml"), "utf-8"))
+
+        .post("/graphql", (body) => {
+          expect(body.variables.projectId).toEqual("PVT_kwDOB8o1Ys4APwEY")
+          expect(body.variables.contentId).toEqual("PR_kwDOJWgzKM5RgLBD")
+          return true;
+        })
+        .reply(200, responseAddItemToProj)
+
+        .post("/graphql", (body) => {
+          expect(body.variables.projectId).toEqual("PVT_kwDOB8o1Ys4APwEY")
+          expect(body.variables.itemId).toEqual("sgrdlmgwrskj")
+          expect(body.variables.date).toEqual("2023-05-27T15:49:07Z")
+          expect(body.variables.fieldId).toEqual("PVTF_lADOB8o1Ys4APwEYzgKDkus")
+          return true;
+        })
+        .reply(200, responseUpdateDateField)
+
+      await probot.receive({ name: "pull_request.opened", payload: payloadPullRequestOpened });
+      expect(mock.pendingMocks()).toStrictEqual([]);
+    });
 
   afterEach(() => {
     nock.cleanAll();
