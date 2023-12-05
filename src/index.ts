8cac2a8ba1fdefef118ef3b52b320af315338446
Mikołaj Makowski <308125@o365.stud.umk.pl>
2023-12-05 13:09:25 +0100

    Move reminder fature to other branch

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { Probot } from "probot";
 import Behaviour from "./behaviours/behaviour";
 import CreateProjectOnTeamCreated from "./behaviours/createProjectOnTeamCreated";
 import CloseProjectOnTeamDeleted from "./behaviours/closeProjectOnTeamDeleted";
+import SendRemindersOnSchedule from "./behaviours/sendRemindersOnSchedule";
 import UpdateProjectOnPullRequestOpened from "./behaviours/updateProjectOnPullRequestOpened";
 import UpdateProjectOnReviewSubmitted from "./behaviours/updateProjectOnReviewSubmitted";
 import LogDebugOnAny from "./behaviours/logDebugOnAny";
@@ -12,7 +13,7 @@ const behaviours: Behaviour[] = [
   new CloseProjectOnTeamDeleted,
   new UpdateProjectOnPullRequestOpened,
   new UpdateProjectOnReviewSubmitted,
-  // new SendRemindersOnSchedule,
+  new SendRemindersOnSchedule,
   new LogDebugOnAny,
 ];
 
